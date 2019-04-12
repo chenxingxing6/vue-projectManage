dfe53d8c2152c0a3c49051c3442c6fdd2fbd7492
lanxinghua <lanxinghua@2dfire.com>
2019-04-12 21:41:00 +0800

    mock数据改为本地服务

--- src/api/mock.js
+++ src/api/mock.js
@@ -22,8 +22,6 @@ export function getNotice(page, pageSize) {
 }
 
 
-
-
 /*获取文件列表*/
 export function getFiles(data) {
     return $http.post('/getFiles', data);
@@ -36,51 +34,51 @@ export function getFileLog(data) {
 
 /*获取关注用户列表*/
 export function getFollowUser(data) {
-    return $http.post('/getFollowUser');
+    return $http.post('/getFollowUser', data);
 }
 
 
 /*获取分享列表*/
 export function getShares(data) {
-    return $http.get('/getShares');
+    return $http.get('/getShares', data);
 }
 
 
 /*获取资源列表*/
 export function getSource(data) {
-    return $http.get('/getSource');
+    return $http.get('/getSource', data);
 }
 
 /*文件重命名*/
 export function fileRename(data) {
-    return $http.post('/file/rename');
+    return $http.post('/file/rename', data);
 }
 
 /*获取操作记录*/
 export function getOperateLog(data) {
-    return $http.post('/getOperateLog');
+    return $http.post('/getOperateLog', data);
 }
 
 
 /*获取首页数据*/
 export function getIndexData(data) {
-    return $http.post('/getIndexData');
+    return $http.post('/getIndexData', data);
 }
 
 
 /*分享查询分享的用户*/
 export function findUser(data) {
-    return $http.post('/findUser');
+    return $http.post('/findUser', data);
 }
 
 /*分享给用户*/
 export function shareToUser(data) {
-    return $http.post('/fileRename');
+    return $http.post('/fileRename', data);
 }
 
 /*创建分享链接*/
 export function createShareUrl(data) {
-    return $http.post('/createShareUrl');
+    return $http.post('/createShareUrl', data);
 }
 
 
